b22b350c83be0e9a6e9e165b2cf84f15a9b88715
Đông Nghi <hoangvodonghi@gmail.com>
2025-10-30 19:46:33 +0700

    Update Program.cs

--- CareNest_Appointment.API/Program.cs
+++ CareNest_Appointment.API/Program.cs
@@ -33,7 +33,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddHttpContextAccessor();
 // Lấy DatabaseSettings từ configuration
-DatabaseSettings dbSettings = builder.Configuration.GetSection("DatabaseSettings").Get<DatabaseSettings>()!;
+
+var config = builder.Configuration;
+DatabaseSettings dbSettings = new DatabaseSettings
+{
+    Ip       = config["DB_HOST"] ?? config["DatabaseSettings:Ip"],
+    Port     = int.TryParse(config["DB_PORT"], out var port) ? port : (config.GetSection("DatabaseSettings").GetValue<int?>("Port") ?? 5432),
+    User     = config["DB_USER"] ?? config["DatabaseSettings:User"],
+    Password = config["DB_PASSWORD"] ?? config["DatabaseSettings:Password"],
+    Database = config["DB_NAME"] ?? config["DatabaseSettings:Database"]
+};
 dbSettings.Display();
 string connectionString = dbSettings?.GetConnectionString();
 
